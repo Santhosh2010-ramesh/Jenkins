c427cc175935cd9aa28fb174871ace1c5db21761
Santhosh2010-ramesh <68174618+Santhosh2010-ramesh@users.noreply.github.com>
2025-02-20 12:51:22 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,8 @@
-# tests/test_example.py
+# test_app.py
+from app import hello_world
+import pytest
 
-from app import greet
-
-def test_greet():
-    assert greet("Jenkins") == "Hello, Jenkins!"
+def test_hello_world(capfd):
+    hello_world()
+    captured = capfd.readouterr()
+    assert captured.out == "Hello, World!\n"
