e096b4dec560d019c6778e86e412cb85a42d490f
Santhosh2010-ramesh <68174618+Santhosh2010-ramesh@users.noreply.github.com>
2025-03-27 17:13:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from datetime import datetime
-
-def mark_attendance(name):
-    now = datetime.now()
-    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-
-    with open("attendance_log.txt", "a") as file:
-        file.write(f"{name} - {formatted_time}\n")
-
-    print(f"Attendance marked for {name} at {formatted_time}")
-
-user_name = input("Enter your name: ")
-mark_attendance(user_name)
+def hello_world():
+    print("Hello, World!")
+if __name__="__main__":
+    hello_world()
